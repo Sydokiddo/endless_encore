64c60a5dc8e1fa2baa86d7d0d529098f1266532e
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2022-05-08 03:43:39 -0700

    - Added Recipes for Peroratite Tools + Block

--- src/main/java/net/sydokiddo/endlessencore/mixin/LivingEntityMixin.java
+++ src/main/java/net/sydokiddo/endlessencore/mixin/LivingEntityMixin.java
@@ -1,16 +1,24 @@
 package net.sydokiddo.endlessencore.mixin;
 
+import net.minecraft.advancement.criterion.ItemDurabilityChangedCriterion;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
+import net.sydokiddo.endlessencore.item.custom_items.SickleItem;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
+    @Shadow protected int itemUseTimeLeft;
     private int elytrabounce$timer = 0;
 
     public LivingEntityMixin(EntityType<?> type, World world) {
