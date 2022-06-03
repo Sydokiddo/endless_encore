30a9c955f24d513658392539199d876489fa3dbe
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2022-06-03 00:27:40 -0700

    - Elytra is now forcefully closed upon taking damage

---
+++ src/main/java/net/sydokiddo/endlessencore/mixin/ElytraCloseOnDamageMixin.java
@@ -0,0 +1,21 @@
+package net.sydokiddo.endlessencore.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public class ElytraCloseOnDamageMixin {
+    @Inject(at = @At("RETURN"), method = "damage")
+    private void onDamage(DamageSource source, float amount, CallbackInfoReturnable info) {
+        LivingEntity player = (LivingEntity) (Object) this;
+        if (player instanceof ServerPlayerEntity && player.isFallFlying() && !((ServerPlayerEntity) player).isCreative()) {
+            ((ServerPlayerEntity) player).stopFallFlying();
+        }
+    }
+}
