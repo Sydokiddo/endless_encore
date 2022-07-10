0dda608812087f59145b807b9f436a587fc26096
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2022-07-10 02:02:40 -0700

    Changed Elytra repair ingredient to Chitin

---
+++ src/main/java/net/sydokiddo/endlessencore/mixin/elytra_tweaks/ElytraChitinRepairingMixin.java
@@ -0,0 +1,20 @@
+package net.sydokiddo.endlessencore.mixin.elytra_tweaks;
+
+import net.minecraft.item.ElytraItem;
+import net.minecraft.item.ItemStack;
+import net.sydokiddo.endlessencore.item.ModItems;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+// Changes the Elytra repair ingredient to Chitin
+
+@Mixin(ElytraItem.class)
+public class ElytraChitinRepairingMixin {
+
+    @Inject(at = @At("RETURN"), method = "canRepair", cancellable = true)
+    private void canRepair(ItemStack stack, ItemStack ingredient, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(ingredient.isOf(ModItems.CHITIN));
+        }
+    }
