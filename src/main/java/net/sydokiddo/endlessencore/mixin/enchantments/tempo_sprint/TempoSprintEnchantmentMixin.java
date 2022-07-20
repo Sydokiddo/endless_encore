bf5cf8c1e581bc23c908b6b85e36219ef116f116
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2022-07-20 00:52:33 -0700

    Ender Dragon is now immune to explosive damage, and health buffed to 400

--- src/main/java/net/sydokiddo/endlessencore/mixin/enchantments/tempo_sprint/TempoSprintEnchantmentMixin.java
+++ src/main/java/net/sydokiddo/endlessencore/mixin/enchantments/tempo_sprint/TempoSprintEnchantmentMixin.java
@@ -1,33 +1,76 @@
 package net.sydokiddo.endlessencore.mixin.enchantments.tempo_sprint;
 
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.ai.attributes.*;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.sydokiddo.endlessencore.enchantment.ModEnchantments;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import java.util.UUID;
 
-@Mixin(Player.class)
-public class TempoSprintEnchantmentMixin {
+// Highly work in progress!
+// Why it no work :(
 
-    @Inject(method = "getSpeed", at = @At("RETURN"))
-    private void tempoSprint(CallbackInfoReturnable<Float> cir) {
+@SuppressWarnings("ALL")
+
+// Currently disabled!
+
+@Nullable
+@Mixin(LivingEntity.class)
+public abstract class TempoSprintEnchantmentMixin {
+
+    @Nullable
+    private final AttributeMap attributes;
+    private static final UUID SPEED_MODIFIER_TEMPO_SPRINT_UUID = UUID.fromString("662A6B8D-DA3E-4C1C-8813-96EA6097278D");
+
+    public TempoSprintEnchantmentMixin(EntityType<? extends LivingEntity> entityType) {
+        this.attributes = new AttributeMap(DefaultAttributes.getSupplier(entityType));
+    }
+
+    @Inject(method = "baseTick", at = @At("RETURN"))
+    private void tempoSprint(CallbackInfo ci) {
         LivingEntity player = (LivingEntity) (Object) this;
-        int tempoSprintLevel = Math.max(0, Math.max(EnchantmentHelper.getItemEnchantmentLevel(ModEnchantments.TEMPO_SPRINT, player.getItemBySlot(EquipmentSlot.LEGS)),
-        EnchantmentHelper.getItemEnchantmentLevel(ModEnchantments.TEMPO_SPRINT, player.getItemBySlot(EquipmentSlot.LEGS))));
+        EnchantmentHelper.getItemEnchantmentLevel(ModEnchantments.TEMPO_SPRINT, player.getItemBySlot(EquipmentSlot.LEGS));
         if (player instanceof ServerPlayer && player.isSprinting()) {
-            if (tempoSprintLevel == 1) {
-                player.setSpeed(1.0f);
+            this.tryAddTempoSprint();
+        }
+        if (player instanceof ServerPlayer && !player.isSprinting()) {
+            this.removeTempoSprint();
+        }
+    }
+
+    @Nullable
+    public AttributeInstance getAttribute(Attribute attribute) {
+        assert this.getAttributes() != null;
+        return this.getAttributes().getInstance(attribute);
+    }
+
+    public @Nullable AttributeMap getAttributes() {
+        assert this.attributes != null;
+        return this.attributes;
+    }
+
+    protected void tryAddTempoSprint() {
+            LivingEntity player = (LivingEntity) (Object) this;
+            int i = EnchantmentHelper.getEnchantmentLevel(ModEnchantments.TEMPO_SPRINT, player);
+            if (i > 0) {
+                AttributeInstance attributeInstance = this.getAttribute(Attributes.MOVEMENT_SPEED);
+                assert attributeInstance != null;
+                attributeInstance.addTransientModifier(new AttributeModifier(SPEED_MODIFIER_TEMPO_SPRINT_UUID, "Tempo sprint boost", 0.03F * (1.0F + (float)i * 0.35F), AttributeModifier.Operation.ADDITION));
             }
-            if (tempoSprintLevel == 2) {
-                player.setSpeed(2.0f);
-            }
-            if (tempoSprintLevel >= 3) {
-                player.setSpeed(3.0f);
+        }
+
+    protected void removeTempoSprint() {
+        AttributeInstance attributeInstance = this.getAttribute(Attributes.MOVEMENT_SPEED);
+        if (attributeInstance != null) {
+            if (attributeInstance.getModifier(SPEED_MODIFIER_TEMPO_SPRINT_UUID) != null) {
+                attributeInstance.removeModifier(SPEED_MODIFIER_TEMPO_SPRINT_UUID);
             }
         }
     }
---
+++ src/main/java/net/sydokiddo/endlessencore/mixin/entities/ender_dragon/EnderDragonMixin.java
@@ -0,0 +1,35 @@
+package net.sydokiddo.endlessencore.mixin.entities.ender_dragon;
+
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.boss.EnderDragonPart;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+// Ender Dragon Tweaks, will be configurable via a config once I get to that
+
+@Mixin(EnderDragon.class)
+public class EnderDragonMixin {
+
+    // Makes the Ender Dragon immune to explosive damage
+    // No more bed bombing, have fun!
+
+    @Inject(at = @At("HEAD"), method = "hurt(Lnet/minecraft/world/entity/boss/EnderDragonPart;Lnet/minecraft/world/damagesource/DamageSource;F)Z", cancellable = true)
+    public void hurt(EnderDragonPart enderDragonPart, DamageSource damageSource, float f, CallbackInfoReturnable<Boolean> cir) {
+        if (damageSource.isExplosion()) {
+            cir.setReturnValue(false);
+        }
+    }
+
+    // Buffs the Ender Dragon's health from 200 to 400
+
+    @Inject(at = @At("HEAD"), method = "createAttributes", cancellable = true)
+    private static void createAttributes(CallbackInfoReturnable<AttributeSupplier.Builder> cir) {
+        cir.setReturnValue(Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 400.0D));
+    }
+}
